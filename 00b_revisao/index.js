cb0b980d198764923ad78f32eedc6b4710fd84ec
Simara Conceição <contato@simaraconceicao.com>
2019-10-28 13:31:01 -0300

    adiciona solução de exercícios 1 e 2

--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -1,6 +1,6 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
+// [x] adicionar a tag <script> no html
+// [x] verificar se está lendo index.js
 // [] alterar cor de <body>
 // [] escutar o clique do botão
 // [] criar função de alterar a cor usando rgb()
@@ -8,7 +8,31 @@
 
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
-// [] anonymous function
-// [] arrow function
+// [x] remover repetição de código
+// [x] anonymous function
+// [x] arrow function
 // [] template string
+
+console.log("estou lendo")
+const body = document.body
+console.log(body)
+
+const botao = document.getElementById("trocaCor")
+
+const n = Math.random()
+const n2 = n*255
+const int = parseInt(n2)
+console.log(int)
+
+geraInteiro = num => parseInt(Math.random() * num)
+
+
+botao.addEventListener("click", () =>{
+    const r = geraInteiro(255)
+    const g = geraInteiro(255)
+    const b = geraInteiro(255)
+ 
+    body.style.backgroundColor = 'rgb('+ r + ',' + g + ',' +  b+')'
+    
+    console.log(r, g, b)    
+})
