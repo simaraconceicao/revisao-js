09bc639842bfac1746ac2438e2035170e5d3d58c
Simara Conceição <contato@simaraconceicao.com>
2019-10-30 15:08:26 -0300

    novas solucoes

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -2,9 +2,7 @@
 // [] o que fazer depois que receber a resposta
 // [] fazer a requisição com open() e send()
 // [] tratar response
-
-
-
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
+
--- 01d_ajax_giphy_random/index.js
+++ 01d_ajax_giphy_random/index.js
@@ -5,3 +5,29 @@
 // [] transformar o dado recebido
 // [] apresentar <img>
 // [] usar async/await, then(), .catch(), arrow function
+
+
+const request = new XMLHttpRequest()
+
+const url ='http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC&limit=1&offset=0'
+
+request.onreadystatechange = mostraGif;
+request.open('GET', url)
+request.send()
+
+
+function mostraGif() {
+    if (request.readyState === 4 && request.status === 200) {
+        const response = request.response
+        
+        const json =  JSON.parse(response)
+        let url = json.data[0].images.original.url
+
+        let imagem = document.getElementById('imagem')
+        imagem.setAttribute('src', url)
+    } else {
+      
+    }   
+    
+}
+
