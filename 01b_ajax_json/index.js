1e2232f6996bb3a6fb10d65218b5880c2604161c
Simara Conceição <contato@simaraconceicao.com>
2019-10-30 16:54:39 -0300

    minhas mudanças

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -1,11 +1,20 @@
-<<<<<<< HEAD
 // [] instanciar XMLHttpRequest()
 // [] o que fazer depois que receber a resposta
 // [] fazer a requisição com open() e send()
 // [] tratar response
-=======
->>>>>>> 4a9acb5fe1637e0dda7f71909d3b55219fba80cb
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
 
+const url = './data.dados.json'
+const req = new XMLHttpRequest()
+req.open('GET', url)
+req.send()
+
+req.onreadystatechange = pegarDados
+function pegarDados () {
+    const response = req.response
+    const json = JSON.parse(response)
+    console.log(json)
+    
+}
